5b7dfb3efb749871b0273901a75d84e322ec550d
Stephen Aghaulor <saghaulor@gmail.com>
2012-02-23 21:50:28 -0800

    Initialized Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SaghaulorCom::Application.config.secret_token = '14ed809f87dec8766b2e6204fcc7931d53342d688618ac80d932448ce4e6d63c14380dabbc70582fbb3691ecc306879c3ebd5fc4bd7078ae7e59dee607ece441'
