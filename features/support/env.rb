f19347d7ed2ab7eb5b15e52338fa766463a64484
Stephen Aghaulor <saghaulor@gmail.com>
2012-03-01 22:23:59 -0800

    Added capybara to spork prefork

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,7 +7,7 @@ Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
-
+  require 'capybara/rails'
 end
 
 Spork.each_run do
