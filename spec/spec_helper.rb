3a4979c13f0beb06e1bba06e5c6bcc6d3a71e154
Stephen Aghaulor <saghaulor@gmail.com>
2012-03-03 12:46:01 -0800

    Must specify capybara for use with with rpsec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@ Spork.prefork do
   require 'rspec/rails'
   require 'rspec/autorun'
   require 'capybara/rails'
+  require 'capybara/rspec'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
