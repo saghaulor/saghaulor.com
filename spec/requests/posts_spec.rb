e285f143f90aabd8c4ebd17a722b285874237bcb
Stephen Aghaulor <saghaulor@gmail.com>
2012-03-04 01:14:22 -0800

    Added spec and code for created new posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
 describe "Posts" do
+
   describe "GET /posts" do
     it "should display posts" do
       get('/posts')
       response.status.should be(200)
     end
   end
+
+  describe "GET /posts/new" do
+    it "should display a form to create new posts" do
+      get('/posts/new')
+      response.status.should be(200)
+    end
+  end
+
 end
