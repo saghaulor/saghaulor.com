5b95fe0337f36d5e44f64f920304d857ad7a7cf4
Stephen Aghaulor <saghaulor@gmail.com>
2012-03-05 19:51:06 -0800

    Added update, show, destroy request specs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,16 @@ class PostsController < ApplicationController
   def new
 
   end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -16,4 +16,25 @@ describe "Posts" do
     end
   end
 
+  describe "GET /posts/:id" do
+    it "should display the post with id" do
+      get('/posts/:id')
+      response.status.should be(200)
+    end
+  end
+
+  describe "PUT /posts/:id" do
+    it "should update the post with id" do
+      put('/posts/:id')
+      response.status.should be(200)
+    end
+  end
+
+  describe "DESTROY /posts/:id" do
+    it "should destroy the post with id" do
+      delete('/posts/:id')
+      response.status.should be(200)
+    end
+  end
+
 end
