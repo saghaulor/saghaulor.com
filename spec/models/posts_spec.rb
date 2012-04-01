66af6265e69ded99796b84e6c43363393d3cae6f
Stephen Aghaulor <saghaulor@gmail.com>
2012-03-31 19:12:50 -0700

    Started converting to rspec2 and factorygirl 3 syntax

--- spec/models/posts_spec.rb
+++ spec/models/posts_spec.rb
@@ -1,53 +1,57 @@
-describe 'Creating posts' do
+describe 'Posts' do
 
-  it 'should require a non-empty title' do
-    p = FactoryGirl.build(:post, :title => nil)
-    expect {p.save!}.to raise_error
+  describe '.create' do
+
+    it 'should require a non-empty title' do
+      p = build(:post, :title => nil)
+      expect {p.save!}.to raise_error
+    end
+
+    it 'should require a unique title' do
+      Post.create(:title => 'A title to be duplicated', :body => 'Body')
+      p = build(:post, :title => 'A title to be duplicated')
+      expect {p.save!}.to raise_error
+    end
+
+    it 'should require a non-empty body' do
+      p = build(:post, :body => nil)
+      expect {p.save!}.to raise_error
+    end
+
   end
 
-  it 'should require a unique title' do
-    Post.create(:title => 'A title to be duplicated', :body => 'Body')
-    p = FactoryGirl.build(:post, :title => 'A title to be duplicated')
-    expect {p.save!}.to raise_error
+  describe '.update' do
+
+    it 'should require a non-empty title' do
+      p = create(:post)
+      p.title = nil
+      expect {p.save!}.to raise_error
+    end
+
+    it 'should require a non-duplicate title' do
+      Post.create(:title => 'A title to be duplicated', :body => 'Body')
+      p = create(:post)
+      p.title = 'A title to be duplicated'
+      expect {p.save!}.to raise_error
+    end
+
+    it 'should require a non-empty body' do
+      p = create(:post)
+      p.body = nil
+      expect {p.save!}.to raise_error
+    end
+
   end
 
-  it 'should require a non-empty body' do
-    p = FactoryGirl.build(:post, :body => nil)
-    expect {p.save!}.to raise_error
-  end
+  describe '.destroy' do
 
-end
+    it 'should reduce the number of posts by one' do
+      p = create(:post)
+      Post.count.should == 1
+      Post.where(:_id => p.id).delete
+      Post.count.should == 0
+    end
 
-describe 'Updating posts' do
-
-  it 'should require a non-empty title' do
-    p = FactoryGirl.create(:post)
-    p.title = nil
-    expect {p.save!}.to raise_error
-  end
-
-  it 'should require a non-duplicate title' do
-    Post.create(:title => 'A title to be duplicated', :body => 'Body')
-    p = FactoryGirl.create(:post)
-    p.title = 'A title to be duplicated'
-    expect {p.save!}.to raise_error
-  end
-
-  it 'should require a non-empty body' do
-    p = FactoryGirl.create(:post)
-    p.body = nil
-    expect {p.save!}.to raise_error
-  end
-
-end
-
-describe 'Destroying posts' do
-
-  it 'should reduce the number of posts by one' do
-    p = FactoryGirl.create(:post)
-    Post.count.should == 1
-    Post.where(:_id => p.id).delete
-    Post.count.should == 0
   end
 
 end
